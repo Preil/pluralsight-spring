af15f0e34a180ffa382f7be00da9283bcc021651
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-14 08:39:21 +0300

    Customer added.

---
+++ src/main/java/com/pluralsight/model/Customer.java
@@ -0,0 +1,25 @@
+package com.pluralsight.model;
+
+/**
+ * Ilya 14.06.2017.
+ */
+public class Customer {
+    private String firstname;
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
