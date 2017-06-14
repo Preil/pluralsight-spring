37e4ecf325863592020dfb5ebad06327a82bcfc2
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-14 19:20:08 +0300

    customerService bean defined with constructor injection. CustomerServiceImp and applicationContext changed.

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -12,6 +12,10 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public List<Customer> findAll() {
         return customerRepository.findAll();
     }
