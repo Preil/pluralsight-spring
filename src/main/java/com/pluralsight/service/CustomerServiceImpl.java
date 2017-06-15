29a6d57c0f677dd9430be0246cf8224fc034e6be
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-15 13:30:12 +0300

    Autowired setter injection example.

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -13,8 +13,13 @@ import java.util.List;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
+
     private CustomerRepository customerRepository;
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public List<Customer> findAll() {
         return customerRepository.findAll();
