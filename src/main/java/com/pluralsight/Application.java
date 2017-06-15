d002ee08019359ec8968172d5f81b9d486a353e5
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-15 16:52:48 +0300

    Application modified - AnnotationConfigApplicationContext added.

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,14 +1,19 @@
 package com.pluralsight;
 
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Ilya 14.06.2017.
  */
 public class Application {
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
+
         System.out.println(service.findAll().get(0).getLastname());
     }
 }
